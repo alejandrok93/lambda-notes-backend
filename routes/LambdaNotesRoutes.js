62e79049b180e3fc570b1d1b59c2e8e329525d78
alejandrok93 <alejandrok93@gmail.com>
2018-10-29 12:52:32 -0700

    Completed GET endpoint to view list of notes

--- routes/LambdaNotesRoutes.js
+++ routes/LambdaNotesRoutes.js
@@ -1,12 +1,19 @@
 //Initial set up
 const express = require("express");
 const router = express.Router();
+const db = require("../data/data.js");
 
 router.get("/", (req, res) => {
   res.send("hello world");
 });
 
 router.get("/notes", (req, res) => {
-  res.json({ testing: "here are thee notes" });
+  db.get()
+    .then(notes => {
+      if (!notes) {
+        res.status(500).json({ error: "There was an error" });
+      } else res.status(200).json(notes);
+    })
+    .error(err => res.status(500).json(err));
 });
 module.exports = router;
