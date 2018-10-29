99d954ac1f48ada35ea0ca351881cfc354f4d9d2
alejandrok93 <alejandrok93@gmail.com>
2018-10-29 12:54:42 -0700

    Completed GET endpoing to view existing note

--- routes/LambdaNotesRoutes.js
+++ routes/LambdaNotesRoutes.js
@@ -16,4 +16,19 @@ router.get("/notes", (req, res) => {
     })
     .error(err => res.status(500).json(err));
 });
+
+router.get("/notes/:id", (req, res) => {
+  const id = req.params.id;
+  if (!id) {
+    res.status(400).json({ error: "Bad Request" });
+  }
+
+  db.getNote(id)
+    .then(note => {
+      if (!note) {
+        res.status(500).json({ error: "There was an error" });
+      } else res.status(200).json(note);
+    })
+    .catch(err => res.status(500).json(err));
+});
 module.exports = router;
