d112669e6bb9d1e79cabc833c91126a7398eedd3
alejandrok93 <alejandrok93@gmail.com>
2018-10-29 13:01:41 -0700

    Completed POST endpoing to add new note

--- routes/LambdaNotesRoutes.js
+++ routes/LambdaNotesRoutes.js
@@ -10,8 +10,10 @@ router.get("/", (req, res) => {
 router.get("/notes", (req, res) => {
   db.get()
     .then(notes => {
-      if (!notes) {
-        res.status(500).json({ error: "There was an error" });
+      if (!notes || notes.length === 0) {
+        res
+          .status(500)
+          .json({ error: "There was an error retrievin the notes" });
       } else res.status(200).json(notes);
     })
     .error(err => res.status(500).json(err));
@@ -25,10 +27,24 @@ router.get("/notes/:id", (req, res) => {
 
   db.getNote(id)
     .then(note => {
-      if (!note) {
-        res.status(500).json({ error: "There was an error" });
+      if (!note || note.length === 0) {
+        res
+          .status(500)
+          .json({ error: "There was an error retrieving the note" });
       } else res.status(200).json(note);
     })
+    .catch(err => res.status(500).json(err));
+});
+
+router.post("/notes", (req, res) => {
+  const newNote = req.body;
+
+  if (!newNote.title) {
+    res.status(400).json({ error: "Bad Request" });
+  }
+
+  db.insert(newNote)
+    .then(idObj => res.status(201).json({ noteId: idObj[0] }))
     .catch(err => res.status(500).json(err));
 });
 module.exports = router;
