944d9ea866c8d47bd45b1b85490defa418077fe0
alejandrok93 <alejandrok93@gmail.com>
2018-10-31 12:50:39 -0700

    Changed notes table content row from string to text

--- data/migrations/20181029123818_add_notes_table.js
+++ data/migrations/20181029123818_add_notes_table.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("notes", table => {
     table.increments().primary();
     table.string("title").notNullable();
-    table.string("content");
+    table.text("content");
     table.integer("user_id");
     table.timestamp("created_at").defaultTo(knex.fn.now());
   });
